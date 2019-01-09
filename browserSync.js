9a6d14b1d3dceccee153790a51b801e59c2ded9e
alexanderasche <alexander.asche@gmail.com>
2019-01-09 17:53:19 -0500

    browserSync fix

--- browserSync.js
+++ browserSync.js
@@ -1,6 +1,6 @@
 const browserSync = require("browser-sync");
-const BASE_URL = "https://dev-pawsforacause.myshopify.com";
-const PREVIEW_QUERY = "?preview_theme_id=58496254016";
+const BASE_URL = "https://dev-alex.myshopify.com";
+const PREVIEW_QUERY = "?preview_theme_id=39247085666";
 
 browserSync({
   proxy: `${BASE_URL}${PREVIEW_QUERY}`,
