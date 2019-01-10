121154f0c4d748a2277cc9cf4191462ca71272e5
alexanderasche <alexander.asche@gmail.com>
2019-01-10 10:47:09 -0500

    fileWatch fix

--- fileWatch.js
+++ fileWatch.js
@@ -1,15 +1,27 @@
 const watch = require("node-watch");
 const copyfiles = require("copyfiles");
+const folders = [
+  "assets",
+  "config",
+  "layout",
+  "locales",
+  "sections",
+  "snippets",
+  "templates"
+];
 
 watch("src", { recursive: true }, function(evt, name) {
-  let file = name.substr(name.lastIndexOf("/") + 1);
-  let folder = name.substr(
-    name.lastIndexOf("src/") + 4,
-    name.lastIndexOf(file) - 5
-  );
-  if (folder !== "vue") {
-    copyfiles([name, `dist/${folder}`], { up: true }, () =>
-      console.log(`copied ${name}`)
-    );
+  let path = name.replace("src/", "");
+  let folder = path.substr(0, path.indexOf("/"));
+  if (folders.indexOf(folder) >= 0) {
+    if (path.indexOf("templates/customers") >= 0) {
+      copyfiles([name, `dist/${folder}/customers`], { up: true }, () =>
+        console.log(`copied ${folder}/customers`)
+      );
+    } else {
+      copyfiles([name, `dist/${folder}`], { up: true }, () =>
+        console.log(`copied ${name}`)
+      );
+    }
   }
 });
