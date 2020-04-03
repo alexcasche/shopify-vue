af6c1fd1dd5fa11c95b49982fc063abe0e186c54
Alireza Jahandideh <ar.jahandideh@gmail.com>
2020-04-03 04:47:09 -0500

    add sample filter

---
+++ src/vue/filters/index.js
@@ -0,0 +1,9 @@
+const filters = {
+    capitalize: value => {
+        if (!value) return ''
+        value = value.toString()
+        return value.charAt(0).toUpperCase() + value.slice(1)
+    }
+}
+
+export default filters;
